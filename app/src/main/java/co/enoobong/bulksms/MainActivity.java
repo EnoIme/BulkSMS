0cc26c9f43c98d8c696fea0d0ecb4011b38b6da7
Ibanga Enoobong <enoimeibanga@gmail.com>
2016-12-09 21:41:55 +0100

    add .csv support

--- app/src/main/java/co/enoobong/bulksms/MainActivity.java
+++ app/src/main/java/co/enoobong/bulksms/MainActivity.java
@@ -1,6 +1,7 @@
 package co.enoobong.bulksms;
 
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.os.Build;
@@ -11,6 +12,7 @@ import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,9 +20,18 @@ import android.widget.Toast;
 
 import com.github.clans.fab.FloatingActionButton;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import static android.Manifest.permission.READ_CONTACTS;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final int FILE_SELECT_CODE = 1;
+    private static final String TAG = "MainActivity";
     private TextInputEditText phoneNumber, message;
     private FloatingActionButton addFromContacts, addFromCSV;
     private static final int REQUEST_READ_CONTACTS = 0;
@@ -53,11 +64,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void getPhoneNumbersFromContacts(){
         Cursor managedCursor = getContentResolver()
-                .query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, new String[]{ContactsContract.CommonDataKinds.Phone._ID,  ContactsContract.CommonDataKinds.Phone.NUMBER}, null, null, null);
+                .query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                        new String[]{ContactsContract.CommonDataKinds.Phone._ID,  ContactsContract.CommonDataKinds.Phone.NUMBER},
+                        null, null, null);
         StringBuilder builder = new StringBuilder();
         try{
             while (managedCursor.moveToNext()) {
-                builder.append(managedCursor.getString(managedCursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.NUMBER)) + " ");
+                builder.append(managedCursor.getString(managedCursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.NUMBER))).append(" ");
             }
         } finally {
             managedCursor.close();
@@ -66,8 +79,53 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         phoneNumber.setText(builder);
     }
 
-    private void getNumbersFromCSV(){
+    private void getPhoneNumbersFromCSV(String uri){
+        File file = new File(uri);
+        StringBuilder builder = new StringBuilder();
+        InputStream inputStream = null;
+        try{
+            inputStream = new FileInputStream(file);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                builder.append(line).append(" ");
+            }
 
+        } catch (Exception e){
+            Log.e(TAG, e.getMessage());
+        } finally {
+            try{
+                inputStream.close();
+            } catch (IOException e){
+                Log.e(TAG, e.getMessage());
+            }
+        }
+        phoneNumber.setText(builder);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case FILE_SELECT_CODE:
+                if (resultCode == RESULT_OK){
+                    String uri = data.getDataString();
+                    getPhoneNumbersFromCSV(uri);
+            }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void launchFilePicker(){
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/.csv");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        try{
+            startActivityForResult(Intent.createChooser(intent, "Select a .csv file"), FILE_SELECT_CODE);
+        } catch (android.content.ActivityNotFoundException ex){
+            Toast.makeText(this, "Please install a File Manager", Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
@@ -135,8 +193,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 getPhoneNumbersFromContacts();
                 break;
             case R.id.addFromCSV:
+                launchFilePicker();
                 break;
             default:
+                break;
         }
     }
 }
